24ce6bcdcf96b2bf4ddeac7690a46ebd0383d635
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-09-18 15:23:04 +0300

    task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const element = event.currentTarget.elements;
+
+  const email = element.email.value;
+  const password = element.password.value;
+
+  if (email === '' || password === '') {
+    return window.alert('Всі поля потрібно заповнити');
+  }
+
+  const data = {
+    email,
+    password,
+  };
+
+  console.log(data);
+
+  form.reset();
+}
