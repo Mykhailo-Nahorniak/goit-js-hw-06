92a1148328765357c26f3a17de996d0aa8d76a4c
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-09-18 15:35:37 +0300

    Task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector('.change-color');
+const colorValue = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', bgColorChange);
+
+function bgColorChange() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.textContent = body.style.backgroundColor;
+}
