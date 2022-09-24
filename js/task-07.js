e04b196dd9b5bb02f77a92bf458305536edaa609
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-09-24 10:15:32 +0300

    Upd t07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const sizeControl = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
 
+text.style.fontSize = `${sizeControl.value}px`;
 sizeControl.addEventListener('input', () => {
   text.style.fontSize = `${sizeControl.value}px`;
 });
