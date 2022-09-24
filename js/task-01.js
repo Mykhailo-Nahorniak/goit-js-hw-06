dcc801fe50d76dafe44cc02b5b8058bbfd1f3cb2
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-09-24 09:42:36 +0300

    Upd task 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const categoryEl = document.querySelectorAll('li.item');
 console.log('Number of categories:', categoryEl.length);
 
 categoryEl.forEach(category => {
-  console.log('Category:', category.querySelector('h2').textContent);
-  console.log('Elements:', category.querySelectorAll('li').length);
+  console.log('Category:', category.firstElementChild.textContent);
+  console.log('Elements:', category.lastElementChild.childElementCount);
 });
