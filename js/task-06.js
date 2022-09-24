5e4d462c28e2c21b12c3e4a575f38776ab747f3b
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-09-24 10:12:35 +0300

    Upd task 06

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,11 @@ const input = document.querySelector('input');
 input.addEventListener('blur', checkInput);
 
 function checkInput() {
-  if (input.value.trim().length >= 6) {
-    input.classList = 'valid';
+  if (input.value.trim().length === Number(input.dataset.length)) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
   } else {
-    input.classList = 'invalid';
+    input.classList.add('invalid');
+    input.classList.remove('valid');
   }
 }
