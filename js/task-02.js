9bd6194c50f779a393d31e6cb9896826567733f5
Mykhailo-Nahorniak <zasineydveryu@gmail.com>
2022-09-24 09:31:55 +0300

    Upd task 2

--- js/task-02.js
+++ js/task-02.js
@@ -9,9 +9,9 @@ const ingredients = [
 
 const listEl = document.querySelector('#ingredients');
 
-const listItems = ingredients.map(callback => {
+const listItems = ingredients.map(item => {
   const listItem = document.createElement('li');
-  listItem.textContent = `${callback}`;
+  listItem.textContent = `${item}`;
   listItem.classList.add('item');
 
   return listItem;
